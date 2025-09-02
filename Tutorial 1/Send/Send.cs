58d87d6e167d6a4968d0419c0c3615dddf83cad4
Tom1779 <tom.arad.2001@gmail.com>
2025-09-02 15:22:17 -0700

    Added gitignore and programs now work on local network

---
+++ Tutorial 1/Send/Send.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System.Text;
+
+using dotenv.net;
+
+DotEnv.Load();
+
+string? address = Environment.GetEnvironmentVariable("LOCAL_IP_ADDRESS");
+Console.WriteLine($"{address}");
+
+string[] commands = { "add", "subtract", "multiply", "divide" };
+Random rand_comm = new Random();
+int command = rand_comm.Next(0, 4); 
+
+var factory = new ConnectionFactory { HostName = $"{address}" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+//creating queue
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+string message = $"{commands[command]}";
+var body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Tutorial 2/Worker/Worker.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+//Creating queue in case sender hasn't started yet
+await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+    arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    int dots = message.Split('.').Length - 1;
+    await Task.Delay(dots * 1000);
+
+    Console.WriteLine(" [x] Done");
+
+    // here channel could also be accessed as ((AsyncEventingBasicConsumer)sender).Channel
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync("hello", autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
